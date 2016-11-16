89f1778eb83445acba0f16c38b56dcc077a5b8e1
Alexey Ermolaev <descendended@gmail.com>
2016-11-16 18:55:40 +0300

    Created filter service

--- src/scripts/todo-app/todo-filters/todo-filters.service.js
+++ src/scripts/todo-app/todo-filters/todo-filters.service.js
@@ -1,3 +1,44 @@
-/**
- * Created by AErmolaev on 16.11.2016.
- */
+(function(w) {
+	'use strict';
+	
+	angular.module('todoApp')
+		.service('TodoFiltersService', TodoFiltersService);
+	
+	TodoFiltersService.$inject = ['$localStorage', '_', '$q', '$timeout'];
+	
+	function TodoFiltersService($localStorage, _, $q, $timeout) {
+		var service = {
+			todos: $localStorage.todos,
+			filtersCollection: [
+				{name: 'Завершенные', alias: 'completed', active: false},
+				{name: 'Незавершенные', alias: 'not_completed', active: false}
+			],
+			getFilters: getFilters,
+			toggleFilter: toggleFilter,
+			clearFilters: clearFilters
+		};
+		
+		function getFilters() {
+			return this.filtersCollection;
+		}
+		
+		function toggleFilter(alias) {
+			this.filtersCollection.map(function(item) {
+				if(item.alias === alias) {
+					item.active = !item.active;
+				}
+			});
+		}
+		
+		function clearFilters() {
+			this.filtersCollection.map(function(item) {
+				item.active = false;
+			});
+		}
+		
+		return service;
+		
+	}
+	
+})(window);
+
